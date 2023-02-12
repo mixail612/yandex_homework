eb083ff35c6ecc53dc157d91c52be1434a882540
mixail612 <81777690+mixail612@users.noreply.github.com>
2023-02-12 13:03:20 +0300

    WEB.HTML_4

--- main.py
+++ main.py
@@ -66,4 +66,41 @@ def index3():
 """
 
 
+@app.route('/promotion_image/')
+def promotion():
+    return f"""
+<!doctype html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+<link rel="stylesheet" 
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+crossorigin="anonymous">
+</head>
+<body>
+<h2>Жди нас марс.</h2></br>
+<img src="{url_for('static', filename='img/mars.png')}" 
+           alt="здесь должна была быть картинка, но не нашлась"></br>
+<div class="alert alert-dark" role="alert">
+        <h5>Человечество вырастет из детства.</h5>
+</div>
+<div class="alert alert-success" role="alert">
+        <h5>Человечеству мала одна планета.</h5>
+</div>
+<div class="alert alert-secondary" role="alert">
+        <h5>Мы сделаем обитаемыми безжизненные пока планеты.</h5>
+</div>
+<div class="alert alert-warning" role="alert">
+        <h5>И начнем с марса!</h5>
+</div>
+<div class="alert alert-danger" role="alert">
+        <h5>Присоединяйся!</h5>
+</div>
+</body>
+</html>
+"""
+
+
+
 app.run(debug='True', port=8080)
